22977d0d07e8ae003821160919d32ce3d4185ffe
Llich <kir120056@gmail.com>
2022-09-01 21:00:49 +0300

    CarController augment with rest methods

--- tests/Usings.cs
+++
@@ -1,6 +0,0 @@
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using api.Controllers;
-global using datalayer.Models;
-global using datalayer.Repositories;
-global using Microsoft.Extensions.Logging;
-global using Moq;
