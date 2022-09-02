870ec81d04e1974f4566e6149db42ecf8c73bc8e
Llich <kir120056@gmail.com>
2022-09-02 17:54:38 +0300

    ServiceController modified by basic rest method

--- api/Controllers/ServiceController.cs
+++ api/Controllers/ServiceController.cs
@@ -17,7 +17,7 @@ namespace api.Controllers
 
         }
 
-        [HttpGet(Name = "Services")]
+        [HttpGet("getAll", Name = "GetServices")]
         public ActionResult<IEnumerable<Service>> GetServices()
         {
             try
@@ -41,5 +41,84 @@ namespace api.Controllers
                 return BadRequest(ex);
             }
         }
+
+        [HttpGet("getById/{id}", Name = "Service")]
+        public ActionResult<Car> GetCarById(int id)
+        {
+            try
+            {
+                Service service = _serviceRepository.GetItemById(id);
+                if (service == null)
+                {
+                    _logger.LogInformation("Service item is null");
+                    return Ok(service);
+                }
+                _logger.LogInformation("Service is successfully receive");
+                return Ok(service);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to get Service");
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost("createNew", Name = "NewService")]
+        public async Task<ActionResult> CreateCarAsync([FromForm] Service service)
+        {
+            try
+            {
+                await _serviceRepository.AddAsync(service);
+                _logger.LogInformation("Service is successfully create");
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to create Service");
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPut("change", Name = "ChangeService")]
+        public async Task<ActionResult> UpdateCarAsync([FromForm] Service newService)
+        {
+            try
+            {
+                await _serviceRepository.UpdateAsync(newService);
+                Service service = _serviceRepository.GetItemById(newService.Id);
+                if (service == newService)
+                {
+                    _logger.LogInformation("Service is successfully change");
+                    return Ok();
+                }
+                else 
+                {
+                    _logger.LogInformation("Failed to change Service");
+                    return Ok(null);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to update Service");
+                return BadRequest(ex);
+            }
+        }
+        
+        [HttpDelete("delete", Name = "DeleteService")]
+        public async Task<ActionResult> DeleteCarAsync([FromForm] Service service)
+        {
+            try
+            {
+                await _serviceRepository.DeleteAsync(service);
+                _logger.LogInformation("Service is successfully delete");
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to delete Service");
+                return BadRequest(ex);
+            }
+        }
+
     }
 }
