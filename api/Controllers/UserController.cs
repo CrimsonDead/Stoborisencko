059a7533f79b63bfebf9321c28647a7f40a2280c
Llich <kir120056@gmail.com>
2022-09-04 14:13:44 +0300

    Add basic method to UserController Add basic rest methods and Identity methods

--- api/Controllers/UserController.cs
+++ api/Controllers/UserController.cs
@@ -14,7 +14,148 @@ namespace api.Controllers
         {
             _signInManager = signInManager;
             _logger = logger;
-
         }
+
+        [HttpGet("getAll", Name = "GetUsers")]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            try
+            {
+                List<User> comments = _signInManager.UserManager.Users.ToList();
+                if (comments.Count == 0)
+                {
+                    _logger.LogInformation("User list is clear");
+                    return Ok(comments);
+                }
+                else if (comments == null)
+                {
+                    _logger.LogInformation("User list is null");
+                    return Ok(comments);
+                }
+                _logger.LogInformation("User list is successfully receive");
+                return Ok(comments);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to get User list");
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("getById", Name = "User")]
+        public ActionResult<User> GetUserById(string id)
+        {
+            try
+            {
+                User user = _signInManager.UserManager.Users.FirstOrDefault(u => u.Id == id);
+                if (user == null)
+                {
+                    _logger.LogInformation("User item is null");
+                    return Ok(user);
+                }
+                _logger.LogInformation("User is successfully receive");
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to get User");
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost("register", Name = "RegisterUser")]
+        public async Task<ActionResult> CreateUserAsync([FromForm] User user, [FromForm] string password)
+        {
+            try
+            {
+                var result = await _signInManager.UserManager.CreateAsync(user, password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, true);
+                    _logger.LogInformation("User successfully registered");
+                    return Ok();
+                }
+                else
+                {
+                    List<string> errors = new List<string>();
+                    _logger.LogError("Failed to register user");
+                    foreach (var error in result.Errors)
+                    {
+                        _logger.LogError(error.Description);
+                        errors.Add(error.Description);
+                    }
+                    return BadRequest(errors);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to register user");
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost("login", Name = "LoginUser")]
+        public async Task<ActionResult> LoginUserAsync([FromForm] string userName, [FromForm] string password)
+        {
+            try
+            {
+                var result = await _signInManager.PasswordSignInAsync(userName, password, true, false);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User successfully login");
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest("Invalid username or password");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to login user");
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost("logout", Name = "Logout")]
+        public async Task<ActionResult> LogoutUser()
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logout");
+            return Ok();
+        }
+
+        [HttpPut("change", Name = "ChangeUser")]
+        public async Task<ActionResult> UpdateUserAsync([FromForm] User newUser)
+        {
+            try
+            {
+                await _signInManager.UserManager.UpdateAsync(newUser);
+                _logger.LogInformation("Comment is successfully change");
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to update User");
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpDelete("delete", Name = "DeleteUser")]
+        public async Task<ActionResult> DeleteUserAsync([FromForm] User user)
+        {
+            try
+            {
+                await _signInManager.UserManager.DeleteAsync(user);
+                _logger.LogInformation("User is successfully delete");
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to delete User");
+                return BadRequest(ex);
+            }
+        }
+
     }
 }
